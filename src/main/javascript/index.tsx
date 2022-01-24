95ed57aee0d26521d7f82fcba1fbfbc94a78ba54
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-01-24 20:18:55 +0100

    Lay down frontend project

---
+++ src/main/javascript/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2020-2022 EC2U Consortium. All rights reserved.
+ */
+
+import * as React from "react";
+import { render } from "react-dom";
+
+
+render((
+
+	<h1>Hello!</h1>
+
+), document.body.firstElementChild);
