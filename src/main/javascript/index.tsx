1f31c112e5bdf1eeff8e8ae88ccd6c2a444492c9
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-02-05 23:52:22 +0100

    Redeploy

--- src/main/javascript/index.tsx
+++ src/main/javascript/index.tsx
@@ -2,9 +2,9 @@
  * Copyright © 2020-2022 EC2U Consortium. All rights reserved.
  */
 
+import { CardHello } from "hello";
 import * as React from "react";
 import { render } from "react-dom";
-import { CardHello } from "./hello";
 import "./index.css";
 
 
@@ -12,8 +12,8 @@ render((<>
 
     <p>This app is saying…</p>
 
-    <CardHello name="Akhil"/>
-    <CardHello name="Andrea"/>
-    <CardHello name="Alessandro"/>
+    <CardHello name="Akhil" />
+    <CardHello name="Andrea" />
+    <CardHello name="Alessandro" />
 
 </>), document.body.firstElementChild);
