fc335d96ab492d39ebec947097cde5c528342af0
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-03-04 08:11:49 +0100

    Fix SPA fallback serving

--- src/main/java/eu/ec2u/card/ToolLoader.java
+++ src/main/java/eu/ec2u/card/ToolLoader.java
@@ -6,14 +6,22 @@ package eu.ec2u.card;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.TEXT_HTML;
+import static org.springframework.http.MediaType.parseMediaTypes;
+
+import static java.lang.String.format;
+import static java.util.Collections.list;
 
 /**
  * SPA fallback loader
@@ -22,24 +30,23 @@ final class ToolLoader implements HandlerInterceptor {
 
     private static final Pattern ExtensionPattern=Pattern.compile("\\.(\\w+)$");
 
+    private static final Pattern HostPattern=Pattern.compile("\\bhost\\s*=\\s*(?<host>.*)\\s*([,;]|$)");
+    private static final Pattern ProtoPattern=Pattern.compile("\\bproto\\s*=\\s*(?<proto>.*)\\s*([,;]|$)");
+
 
     @Override public boolean preHandle(
 
             final HttpServletRequest request, final HttpServletResponse response, final Object handler
 
-    ) throws ServletException, IOException {
+    ) throws IOException {
 
+        final boolean safe=Set.of("GET", "HEAD").contains(request.getMethod());
         final boolean interactive=parseMediaTypes(request.getHeader("Accept")).contains(TEXT_HTML);
         final boolean route=!ExtensionPattern.matcher(request.getRequestURI()).find();
-        final boolean original=request.getAttribute("javax.servlet.include.request_uri") == null;
 
-        if ( interactive && route && original ) {
+        if ( safe && interactive && route ) {
 
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.setContentType(TEXT_HTML_VALUE);
-            response.setCharacterEncoding("UTF8");
-
-            request.getRequestDispatcher("/index.html").include(request, response);
+            response(response, request(request));
 
             return false;
 
@@ -49,6 +56,72 @@ final class ToolLoader implements HandlerInterceptor {
 
         }
 
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private HttpURLConnection request(final HttpServletRequest request) throws IOException {
+
+        final String proto=Optional.ofNullable(request.getHeader("Forwarded"))
+
+                .map(ProtoPattern::matcher)
+                .filter(Matcher::matches)
+                .map(matcher -> matcher.group("proto"))
+
+                .or(() -> Optional.ofNullable(request.getHeader("X-Forwarded-Proto")))
+
+                .orElseGet(request::getScheme);
+
+        final String host=Optional.ofNullable(request.getHeader("Forwarded"))
+
+                .map(HostPattern::matcher)
+                .filter(Matcher::matches)
+                .map(matcher -> matcher.group("host"))
+
+                .or(() -> Optional.ofNullable(request.getHeader("X-Forwarded-Host")))
+
+                .or(() -> Optional.ofNullable(request.getHeader("Host")))
+
+                .orElseThrow(() ->
+                        new IllegalArgumentException("undefined http <host> header") // unexpected
+                );
+
+
+        final URL url=new URL(format("%s://%s%s", proto, host, "/index.html"));
+
+        final HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+
+        connection.setDoInput(true);
+        connection.setDoOutput(false);
+
+        connection.setRequestMethod(request.getMethod());
+
+        list(request.getHeaderNames()).forEach(name -> list(request.getHeaders(name)).forEach(value ->
+                connection.addRequestProperty(name, value)
+        ));
+
+        return connection;
+    }
+
+    private void response(final HttpServletResponse response, final HttpURLConnection connection) throws IOException {
+
+        connection.connect();
+
+        response.setStatus(connection.getResponseCode());
+
+        connection.getHeaderFields().forEach((name, values) -> values.forEach(value ->
+                response.addHeader(name, value)
+        ));
+
+        try (
+                final InputStream input=connection.getInputStream();
+                final OutputStream output=response.getOutputStream();
+        ) {
+
+            input.transferTo(output);
+
+        }
     }
 
 }
