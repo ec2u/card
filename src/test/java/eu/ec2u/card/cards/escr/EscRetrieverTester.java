9c34df314bc8fe98b8c470ca13ddc74042968a43
Alessandro Ceseno <alessandro.ceseno@unipv.it>
2022-03-23 12:35:27 +0100

    Refactor method name

--- src/test/java/eu/ec2u/card/cards/escr/EscRetrieverTester.java
+++ src/test/java/eu/ec2u/card/cards/escr/EscRetrieverTester.java
@@ -71,7 +71,7 @@ public final class EscRetrieverTester {
 	}
 
 	@Test
-	public void testListStudents() {
+	public void listStudents() {
 
 		try {
 			String responseRetrieveStudents = escRetriever.retrieveStudents();
