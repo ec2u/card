52e8f629fd60888d565c571d2a5c1f64d1e1edc1
Rondla Akhil Reddy <akhilreddy.rondla01@universitadipavia.it>
2022-03-28 15:07:31 +0200

    Add delete dialog

---
+++ src/main/javascript/pages/users/deletedialog.tsx
@@ -0,0 +1,31 @@
+import { Trash, X } from "lucide-react";
+import React from "react";
+import "./deletedialog.css";
+
+export function Deletedialog(props: any) {
+  return (
+    <div className="header">
+      <div className="delete">
+        <div className={"data"}>
+          <Trash size={78} className="trash" />
+
+          <span className="data-header">are you sure ?</span>
+
+        </div>
+
+        <div className="button">
+          <div>
+            <button className="yes" onClick={props.handleyes}>
+              Yes
+            </button>
+          </div>
+          <div>
+            <button className="no" onClick={props.handleno}>
+              No
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
