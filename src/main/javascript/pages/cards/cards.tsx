ff22c37260334ad869036d601c5d9308319246d3
Rondla Akhil Reddy <akhilreddy.rondla01@universitadipavia.it>
2022-03-28 15:08:10 +0200

    Add all cards page

--- src/main/javascript/pages/cards/cards.tsx
+++ src/main/javascript/pages/cards/cards.tsx
@@ -1,5 +1,5 @@
 import { ChevronRight, Plus, Search } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
+import React, { createElement, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import './cards.css';
 
@@ -7,7 +7,7 @@ import './cards.css';
 interface Card {
 
     holderForename: string;
-    holderSurName: string;
+    holderSurname: string;
     expiringDate: Date;
     virtualCardNumber: number;
     id: any;
@@ -19,69 +19,85 @@ interface Card {
 export function VirtualCards() {
 
     const [cards, setCards] = useState<Card[]>([]);
+    const [loading, setLoading] = useState<Boolean>(false);
 
 
     useEffect(() => {
-        fetch('/cards/', {
-            headers: {
-                Accept: 'application/json',
-            }
-        })
-            .then(response => response.json())
-            .then(data => setCards(data.contains))
+        const fetchData = async () => {
+            setLoading(true)
+            await fetch('/cards/', {
+                headers: {
+                    Accept: 'application/json',
+                }
+            })
+                .then(response => response.json())
+                .then(data => setCards(data.contains))
+
+            setLoading(false)
+        }
+        fetchData();
     }, [])
 
-    return (
 
-        <div className={"cards"}>
-            <div className={"topnav-cards"}>
-                <span> Cards</span>
-                <span>
-                    <Link to="/cards/add">
+    return createElement('card-cards', {},
+        <>
+            < div className={"cards"} >
+                <div className={"topnav-cards"}>
+                    <span> Cards</span>
+                    <a title="addcard" href='/cards/add'>
+
                         <Plus size={38} className={"button-plus"} />
-                    </Link>
-                </span>
-            </div>
 
-            <div className="grid-container-cards">
-                <table>
-                    <thead>
-                        <tr className='tr-cards'>
-                            <th>forename</th>
-                            <th>surname</th>
-                            <th>expiry date</th>
-                            <th>card number</th>
-                            <th>
-                                <Search size={28} color="black" className='button-search' />
-                            </th>
+                    </a>
+                </div>
+
+                <div className="grid-container-cards">
+                    <table>
+                        <thead>
+                            <tr className='tr-cards'>
+                                <th>forename</th>
+                                <th>surname</th>
+                                <th>expiry date</th>
+                                <th>card number</th>
+                                <th>
+                                    <Search size={28} color="black" className='button-search' />
+                                </th>
 
 
-                        </tr>
-                    </thead>
+                            </tr>
+                        </thead>
 
-                    <hr className={"solid-cards"} />
+                        <hr className={"solid-cards"} />
+                        {loading ? (<div className='spinner'></div>) : (
+                            <tbody>
+                                {cards.map((card) => {
+                                    return (
+                                        <tr key={card.id} className='tr-cards'>
+                                            <td>{card.holderForename}</td>
+                                            <td>{card.holderSurname}</td>
+                                            <td>{card.expiringDate}</td>
+                                            <td>{card.virtualCardNumber}</td>
 
-                    <tbody>
-                        {cards.map((card) => {
-                            return (
-                                <tr key={card.id} className='tr-cards'>
-                                    <td>{card.holderForename}</td>
-                                    <td>{card.holderSurName}</td>
-                                    <td>{card.expiringDate}</td>
-                                    <td>{card.virtualCardNumber}</td>
+                                            <td>
+                                                <Link to={`${card.id}`}>
+                                                    <ChevronRight size={40} className={"button-arrow"} />
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>)}
 
-                                    <td>
-                                        <Link to={`${card.id}`}>
-                                            <ChevronRight size={40} className={"button-arrow"} />
-                                        </Link>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
 
+                    </table>
+
+                </div>
+            </div >
+
+        </>
     )
+
+
+
+
 }
