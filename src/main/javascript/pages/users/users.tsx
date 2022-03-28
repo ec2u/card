975a01507cbe8272f6bdc71eeb325f4b0c9777fb
Rondla Akhil Reddy <akhilreddy.rondla01@universitadipavia.it>
2022-03-28 15:07:05 +0200

    Add card users get all page

--- src/main/javascript/pages/users/users.tsx
+++ src/main/javascript/pages/users/users.tsx
@@ -1,82 +1,102 @@
 import { ChevronRight, Plus, Search } from "lucide-react";
 import React, { createElement, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./users.css";
+import './users.css';
 
-interface Users {
-  contains: User[];
-}
+
 
 interface User {
-  admin: boolean;
-
-  forename: string;
-  surname: string;
-  email: string;
-  id: any;
+  readonly admin: boolean;
+  readonly forename: string;
+  readonly surname: string;
+  readonly email: string;
+  readonly id: any;
 }
 
 export function CardUsers() {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<Boolean>(false);
+
 
   useEffect(() => {
-    fetch("/users/", {
-      // !!! factor useContext hook
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setUsers(data.contains));
+
+    const fetchData = async () => {
+      setLoading(true)
+      await fetch("/users/", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => setUsers(data.contains));
+      setLoading(false)
+    }
+    fetchData();
   }, []);
 
-  return (
-    <div className={"users"}>
-      <div className={"topnav"}>
-        <span> Users</span>
-        <span>
-          <Link to="/users/add">
-            <Plus size={38} className={"button-plus"} />
-          </Link>
-        </span>
-      </div>
 
-      <div className="grid-container">
+
+  return createElement('card-users', {},
+    <>
+
+
+      <header>
+
+
+        <a> Users </a>
+        <a href='/users/add' title="add"> <Plus size={40} className={"button-plus"} /> </a>
+
+
+      </header>
+
+
+      <footer>
+
         <table>
+
           <thead>
+
             <tr>
+
               <th>forename</th>
               <th>surname</th>
               <th>email</th>
-
               <th>
                 <Search size={28} className={"button"} />
               </th>
+
             </tr>
+
           </thead>
 
-          <hr className={"solid"} />
+          {loading ?
 
-          <tbody>
-            {users.map((user) => {
-              return (
-                <tr key={user.id}>
-                  <td>{user.forename}</td>
-                  <td>{user.surname}</td>
-                  <td>{user.email}</td>
+            (<div className="spinner"></div>) : (
+              <tbody>
+                {users.map((user) => {
 
-                  <td>
-                    <Link to={`${user.id}`}>
-                      <ChevronRight size={40} className={"button"} />
-                    </Link>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
+                  return (
+                    <tr key={user.id} >
+
+                      <td>{user.forename}</td>
+                      <td>{user.surname}</td>
+                      <td>{user.email}</td>
+                      <td>
+                        <Link to={`${user.id}`}>
+                          <ChevronRight size={40} className={"button"} />
+                        </Link>
+                      </td>
+
+                    </tr>
+                  );
+                })}
+              </tbody>
+
+            )}
         </table>
-      </div>
-    </div>
+      </footer>
+    </>
   );
+
 }
 
