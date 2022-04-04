b2194b5ba14a3207ca46747304a8c8ffa0116caa
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-04-04 13:02:53 +0200

    Refactor Loader

--- src/main/java/eu/ec2u/card/filters/Loader.java
+++ src/main/java/eu/ec2u/card/filters/Loader.java
@@ -18,13 +18,20 @@ package eu.ec2u.card.filters;
 
 import com.sun.net.httpserver.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Locale;
 import java.util.Optional;
 
 import static eu.ec2u.card.Handler.*;
 
 import static java.lang.String.format;
+import static java.net.http.HttpClient.newHttpClient;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class Loader extends Filter {
 
@@ -34,61 +41,101 @@ public final class Loader extends Filter {
 
     @Override public void doFilter(final HttpExchange exchange, final Chain chain) throws IOException {
 
-        if ( isSafe(exchange) && isInteractive(exchange) && !isAsset(exchange) ) {
+        if ( isSafe(exchange) ) {
 
-            final Headers headers=exchange.getRequestHeaders();
+            if ( isAsset(exchange) ) {
 
-            final String proto=Optional.ofNullable(headers.getFirst("X-Forwarded-Proto"))
-                    .or(() -> Optional.ofNullable(exchange.getProtocol())
-                            .map(p -> p.substring(p.indexOf('/')))
-                            .map(p -> p.toLowerCase(Locale.ROOT))
-                    )
-                    .orElse("http");
+                asset(exchange);
 
-            final String host=Optional.ofNullable(headers.getFirst("X-Forwarded-Host"))
-                    .or(() -> Optional.ofNullable(headers.getFirst("Host")))
-                    .orElse("localhost");
+            } else if ( isInteractive(exchange) ) {
+
+                fallback(exchange);
+
+            } else {
+
+                chain.doFilter(exchange);
+
+            }
+
+        }
+
+    }
 
 
-            final String index=format("%s://%s/index.html", proto, host);
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            // !!! replace
+    private void asset(final HttpExchange exchange) throws IOException {
 
-            exchange.getResponseHeaders().set("Location", index);
-            exchange.sendResponseHeaders(302, -1);
+        // !!! prevent tree walking
 
-            //final HttpRequest request=HttpRequest.newBuilder().GET()
-            //        .uri(URI.create(index))
-            //        .build();
-            //
-            //// !!! prevent loops
-            //
-            //try {
-            //
-            //    final HttpResponse<String> response=newHttpClient().send(request, BodyHandlers.ofString());
-            //
-            //    // !!! network error handling
-            //
-            //    exchange.getResponseHeaders().set("Content-Type", "text/html");
-            //
-            //    exchange.sendResponseHeaders(OK, 0);
-            //
-            //    try (
-            //            final OutputStream output=exchange.getResponseBody();
-            //    ) {
-            //
-            //        output.write(response.body().getBytes(UTF_8));
-            //
-            //    }
-            //
-            //} catch ( final InterruptedException e ) {
-            //    throw new RuntimeException(e); // !!! handle
-            //}
+        final URL asset=Loader.class.getResource("/static"+exchange.getRequestURI());
+
+
+        if ( asset == null ) {
+
+            exchange.sendResponseHeaders(NotFound, -1);
 
         } else {
 
-            chain.doFilter(exchange);
+            exchange.sendResponseHeaders(OK, 0);
+            //exchange.getResponseHeaders().set("Content-Type", "text/html");
 
+            try (
+                    final InputStream input=asset.openStream();
+                    final OutputStream output=exchange.getResponseBody()
+            ) {
+
+                input.transferTo(output);
+
+            }
+
+        }
+    }
+
+    private void fallback(final HttpExchange exchange) throws IOException {
+
+        final Headers headers=exchange.getRequestHeaders();
+
+        final String proto=Optional.ofNullable(headers.getFirst("X-Forwarded-Proto"))
+                .or(() -> Optional.ofNullable(exchange.getProtocol())
+                        .map(p -> p.substring(p.indexOf('/')))
+                        .map(p -> p.toLowerCase(Locale.ROOT))
+                )
+                .orElse("http");
+
+        final String host=Optional.ofNullable(headers.getFirst("X-Forwarded-Host"))
+                .or(() -> Optional.ofNullable(headers.getFirst("Host")))
+                .orElse("localhost");
+
+
+        final String index=format("%s://%s/", proto, host);
+
+        final HttpRequest request=HttpRequest.newBuilder().GET()
+                .uri(URI.create(index))
+                .build();
+
+        // !!! prevent loops
+
+        try {
+
+            final HttpResponse<String> response=newHttpClient().send(request, BodyHandlers.ofString());
+
+            // !!! network error handling
+
+            exchange.getResponseHeaders().set("Content-Type", "text/html");
+
+            exchange.sendResponseHeaders(OK, 0);
+
+            try (
+                    final OutputStream output=exchange.getResponseBody();
+            ) {
+
+                output.write(response.body().getBytes(UTF_8));
+
+            }
+
+        } catch ( final InterruptedException e ) {
+            throw new RuntimeException(e); // !!! handle
         }
 
     }
