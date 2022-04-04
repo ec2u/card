40d281a77be39c8cb40b8fd773b52f892286fe11
Andrea Galmuzzi <andrea.galmuzzi01@universitadipavia.it>
2022-04-04 22:34:47 +0200

    Add updated domain objects for data transfer

---
+++ src/main/java/eu/ec2u/card/escr/CardTransfer.java
@@ -0,0 +1,23 @@
+package eu.ec2u.card.escr;
+
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@SuppressWarnings("ALL")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class CardTransfer {
+
+	private String europeanStudentCardNumber;
+	private String europeanStudentIdentifier;
+	private Long picInstitutionCode;
+	private String emailAddress;
+	private LocalDateTime expiryDate;
+	private Optional<Integer> cardType;
+	private Optional<String> cardUid;
+
+}
