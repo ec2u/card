93d9446d8c8a37fccefec9ab258854125d2e1a02
Rondla Akhil Reddy <akhilreddy.rondla01@universitadipavia.it>
2022-04-27 20:25:16 +0200

    Add disable feature

--- src/main/javascript/pages/tokens/edittoken.tsx
+++ src/main/javascript/pages/tokens/edittoken.tsx
@@ -21,7 +21,7 @@ export function EditToken() {
     } as Token);
     const [dialog, setDialog] = useState<Boolean>(false);
     const [clicked, setClicked] = useState<Boolean>(false);
-    const [disable, setDisable] = useState<Boolean>(false);
+    const [disable, setDisable] = useState<Boolean>(true);
     const [loading, setLoading] = useState<Boolean>(false);
 
     const { id } = useParams();
