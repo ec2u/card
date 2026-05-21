4c1a7d87f265b0c88effcca7ddd86fab390f9704
Alessandro Bollini <alessandro.bollini@metreeca.com>
2026-05-21 16:55:34 +0200

    docs: Bump copyright year to 2026

--- code/javascript/env.d.ts
+++ code/javascript/env.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020-2022 Metreeca srl
+ * Copyright © 2020-2026 Metreeca srl
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
